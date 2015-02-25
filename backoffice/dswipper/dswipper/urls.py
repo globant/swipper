2be6c1cedacdf5d9331e5be6cb9ac463dbb167f3
Sebastian Bassi <sbassi@genesdigitales.com>
2015-02-24 23:31:06 -0300

    form to set country and data size

--- backoffice/dswipper/dswipper/urls.py
+++ backoffice/dswipper/dswipper/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'swipperbo.views.home', name='home'),
-    url(r'^country/(?P<cc>[a-z]+)/', 'swipperbo.views.country', name='country'),
+    #url(r'^country/(?P<cc>[a-z]+)/', 'swipperbo.views.country', name='country'),
     # url(r'^blog/', include('blog.urls')),
     #url(r'',),
     url(r'^admin/', include(admin.site.urls)),
--- backoffice/dswipper/swipperbo/views.py
+++ backoffice/dswipper/swipperbo/views.py
@@ -8,13 +8,38 @@ import json
 
 
 def home(request):
-    return render(request,"index.html")
+    if request.GET:
+        cc = request.GET['country']
+        size = request.GET['points']
+        print size
+        countryd = {'ar':'Argentina', 'uy':'Uruguay'}
+        country = countryd.get(cc,'Argentina')
+
+        requrl = settings.API_BASE_URL + 'places/count/'
+        response = urllib2.urlopen(requrl)
+        places_nmbr = json.loads(response.read())['count']
+        requrl = settings.API_BASE_URL + 'places?filter={"where":{"Country":"%s"},"limit":%s}'%(country, size)
+
+        response = urllib2.urlopen(requrl)
+        json_res = json.loads(response.read())
+
+        context = {'data':json_res, 'country':country, 'size':size}
+
+
+        return render(request, "country.html", context)
+
+
+    else:
+        return render(request,"index.html")
 
 
 def country(request,cc):
 
-    countryd = {'ar':'Argentina', 'uy':'Uruguay'}
-    country = countryd.get(cc,'Argentina')
+
+
+    # for pagination
+    # Get ammount of records
+
 
     requrl = settings.API_BASE_URL + 'places?filter={"where":{"Country":"%s"},"limit":50}'%country
 
